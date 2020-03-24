424bfc92fc44e4c9960a2f57ebbf824be7430f84
Vishaish Pandita <visheshpandita9899@gmail.com>
2020-03-24 13:05:47 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+//jhgk
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) 
+  },
+  { 
+    path: 'order', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) 
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
